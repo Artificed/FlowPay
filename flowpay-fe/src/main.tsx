30fcc0bbd53cf5c554cf4e41437592a5e1e6f6e3
Artificed <paultsai.id@gmail.com>
2026-03-20 17:23:47 +0700

    refactor(theme): removed theme provider

--- flowpay-fe/src/main.tsx
+++ flowpay-fe/src/main.tsx
@@ -3,15 +3,12 @@ import { createRoot } from "react-dom/client"
 
 import "./index.css"
 import App from "./App.tsx"
-import { ThemeProvider } from "@/providers/ThemeProvider"
 import { AuthProvider } from "@/providers/AuthProvider"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ThemeProvider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>
 )
